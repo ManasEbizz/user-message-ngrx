54a089883e3557e385eea46c641970d494717cb9
ManasEbizz <manas@ebizzinfotech.com>
2023-01-13 19:21:39 +0530

    MatCardModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,12 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
   },
+
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,9 +4,9 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
+import { MatCardModule } from '@angular/material/card';
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, MatToolbarModule],
+  imports: [CommonModule, HomeRoutingModule, MatToolbarModule, MatCardModule],
 })
 export class HomeModule {}
